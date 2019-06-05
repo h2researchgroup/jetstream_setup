7195ecdf459d571d90c7090a3353c9837eceb37c
Aaron Culich <aculich@gmail.com>
2019-06-04 20:31:27 -0700

    use up-to-date URL for globus download

--- install-globus.sh
+++ install-globus.sh
@@ -5,6 +5,6 @@ GLOBUS_VERSION=$(tar ztvf $TARBALL | head -1 | cut -d" " -f 10 | cut -d- -f2 | c
 
 ## install Globus Personal Connect
 VERSION=$GLOBUS_VERSION
-wget --directory-prefix=/usr/local https://s3.amazonaws.com/connect.globusonline.org/linux/stable/$TARBALL
+wget --directory-prefix=/usr/local https://downloads.globus.org/globus-connect-personal/linux/stable/$TARBALL
 (cd /usr/local && tar zxvf $TARBALL)
 (cd /usr/local/bin && ln -s ../globusconnectpersonal-$VERSION/globusconnect)
