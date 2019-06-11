f3c648104b1b742562cc7b0fecdb8952746953e5
root <root@js-169-213.jetstream-cloud.org>
2019-06-11 13:53:38 -0400

    fine-tuning user authorizations

--- init_RStudio.sh
+++ init_RStudio.sh
@@ -2,20 +2,18 @@
 conda update -n base conda
 ezj -R # Note: This starts a Jupyter server. Kill it with Ctrl-C
 export PATH="/opt/anaconda3/bin:$PATH"
-chown ${USER}:iplant-everyone /opt/anaconda3/ -R
+chown $(id -u):$(id -g) /opt/anaconda3/ -R
 echo "export RSTUDIO_WHICH_R='/opt/anaconda3/bin/R'" >> ~/.bash_profile
 apt-get install gdebi-core
 
 wget https://download2.rstudio.org/server/trusty/amd64/rstudio-server-1.2.1335-amd64.deb 
 # Update above URL to latest Ubuntu-specific version  here: https://www.rstudio.com/produc$
-sudo gdebi rstudio-server-1.2.1335-amd64.deb # First try won't work, but it will work after this
+sudo gdebi rstudio-server-1.2.1335-amd64.deb # First try will fail because we haven't told RStudio-Server which R to use. We start Server again below
 sudo sh -c 'echo "rsession-which-r=/opt/anaconda3/bin/R" >> /etc/rstudio/rserver.conf'
 conda install -c conda-forge r-stm  # Install STM R package (this may fail, working out dependency conflicts)
 
-# Restart terminal, then start RStudio-Server with these commands:
-#sudo gdebi rstudio-server-1.2.1335-amd64.deb
-#rm rstudio-server-1.2.1335-amd64.deb
-#sudo rstudio-server start
+# Restart terminal, then start RStudio-Server:
+sudo rstudio-server start
 
 # Now you can login to the VM's RStudio-Server using: VM_IP.jetstream-cloud.org:8787
 
