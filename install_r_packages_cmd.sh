811e81b82296dc0bbc3d13597e19b05972f4b4b2
Jaren Haber <jhaber@berkeley.edu>
2019-06-26 15:03:30 -0400

    install R packages for containers

---
+++ install_r_packages_cmd.sh
@@ -0,0 +1,6 @@
+wget https://cran.r-project.org/src/contrib/geometry_0.4.1.tar.gz
+R CMD INSTALL geometry_0.4.1.tar.gz
+wget http://cran.r-project.org/src/contrib/rsvd_1.0.1.tar.gz
+R CMD INSTALL rsvd_1.0.1.tar.gz
+wget https://cran.r-project.org/src/contrib/tidyverse_1.2.1.tar.gz
+R CMD INSTALL tidyverse_1.2.1.tar.gz
